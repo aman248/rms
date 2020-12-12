585ce652537851d84a5ecea33928ff03d3121154
aman248 <baman248@gmail.com>
2020-12-13 04:29:10 +0530

    ad

--- gadar/settings.py
+++ gadar/settings.py
@@ -26,7 +26,7 @@ MEDIA_URL = '/media/'
 SECRET_KEY = 's$8h*^l@w83=8(1o&s@*-h(()b@r8amb7jn(0dyz8fl3q9-w(o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
